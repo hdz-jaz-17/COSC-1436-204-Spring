54d33450f882edb0f5c953b17d29239d9a4d7b99
hdz-jaz-17 <Hernandez.J099216@stu.SanJac.edu>
2025-03-25 03:08:28 +0000

    2nd

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-       
+
+        System.out.println("My name is Jazmin.");
+
+        System.out.println("My favorite food is sushi.");
+
+        System.out.println("I would like to meet Megan Moroney.");
+
+        System.out.println("My least favorite chore is doing the dishes.");
+
+        System.out.println("I would rather be at the beach.");
+    }
